37834222dd8c3969524fa645c505c1b4efd61005
tylakde <tylakiff@gmail.com>
2026-01-19 19:47:33 +0000

    Wordcounter

--- Bookbot/main.py
+++ Bookbot/main.py
@@ -3,8 +3,15 @@ def get_book_text(path_to_file):
     with open(path_to_file) as f:
         file_contents = f.read()
     return file_contents
+def get_word_count(file_contents):
+    text = file_contents.split()
+    return len(text)
+
+
 def main():
-    frankenstein =  "/home/tyla/workspace/Bookbot/Bookbot/books/books/frankenstein.txt"
+    frankenstein =  "/home/tyla/workspace/Bookbot/books/books/frankenstein.txt"
     book_contents = get_book_text(frankenstein)
     print(book_contents)
+    wordcount = get_word_count(book_contents)
+    print(f"{wordcount} words found.")
 main()
