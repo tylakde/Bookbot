a9ea3e94b9181c46a17cafadf1cf292606fa5d0d
tylakde <tylakiff@gmail.com>
2026-01-21 08:11:24 +0000

    Refactoring 2

--- Bookbot/main.py
+++ Bookbot/main.py
@@ -1,9 +1,10 @@
 
+from stats import get_word_count
+
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
         file_contents = f.read()
     return file_contents
-from stats import get_word_count
 
 
 def main():
